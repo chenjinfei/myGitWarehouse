2df0ae1c4cb1a517be33540ac0cddaec7da7c133
lanou <lan@luozhicong.local>
2016-07-30 17:59:21 +0800

    test

--- myProject/myProject/General/ItemOne/ViewControllers/OneViewController.m
+++ myProject/myProject/General/ItemOne/ViewControllers/OneViewController.m
@@ -23,18 +23,41 @@
     
     AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
     
+    
+    
     NSDictionary *parameters = @{
-                                 @"detail":@1850878,
-                                 @"flag":@0,
-                                 @"prop_num":@10,
-                                 @"user_id":@1850878
+//                                 @"detail":@1,
+//                                 @"flag":@0,
+//                                 @"prop_num":@10,
+//                                 @"private":@2,
+//                                 @"comment_text_content":@"Foch%20",
+                                 @"user_id":@1850878,
+//                                 @"nickname":@"吃",
+//                                 @"next_id":@-1,
+//                                 @"article_id":@105,
+                                   @"mind_note_id":@18035359
                                  };
-    [session POST:APIAllHotNotes parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+    [session POST:APICancelProp parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         NSLog(@"ok === %@", responseObject);
         ULog(@"%@", responseObject);
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
         NSLog(@"%@", error);
     }];
+    
+    
+    
+//    NSDictionary *parameters = @{
+//                                 @"detail":@1850878,
+//                                 @"flag":@0,
+//                                 @"prop_num":@10,
+//                                 @"user_id":@1850878
+//                                 };
+//    [session POST:APIAllHotNotes parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+//        NSLog(@"ok === %@", responseObject);
+//        ULog(@"%@", responseObject);
+//    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+//        NSLog(@"%@", error);
+//    }];
     
 }
 
